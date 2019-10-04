94fc211caf0a9697bcddf3afd89166be7c09163b
Ailan Jiang <ailanjiang@Ailans-MacBook-Pro.local>
2019-10-03 23:56:49 -0400

    final submission for a1

---
+++ q2/www/js/app.js
@@ -0,0 +1,7 @@
+function handleCSExplore(show, hide) {
+  var show = document.getElementById(show);
+  var hide = document.getElementById(hide);
+
+  show.style.display = "block";
+  hide.style.display = "none";
+}
